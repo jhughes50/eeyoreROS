48cefc2296698dc517926dc5bcb9b191b0d99f10
Jason <jasonah@seas.upenn.edu>
2024-06-11 09:51:57 -0400

    remove some dev artifacts

--- etc/bosondev.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-if os.path.exists("/dev/boson_ser") and os.path.exists("/dev/boson_video"):    
-    print("--device /dev/boson_video -v /dev/boson_video:/dev/boson_video --device /dev/boson_ser -v /dev/boson_ser:/dev/boson_ser")
-
